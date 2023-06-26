081564ea4dd6d3b3afb76dfda3b464f2cd8d519b
Ruslan Ismayilov <ruslanismayilovv@icloud.com>
2023-06-25 22:01:58 -0700

    UI Setup

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Ruslan Ismayilov on 6/25/23.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
